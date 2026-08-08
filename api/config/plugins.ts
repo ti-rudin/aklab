9c322fabdb240020fa290d6795f7fa3943f68daa
A.Rudin <ax.rudin@gmail.com>
2026-08-08 23:26:12 +0300

    Исправление: убрать ложное отключение Strapi Upload

--- api/config/plugins.ts
+++ api/config/plugins.ts
@@ -1,9 +1,6 @@
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
-  upload: {
-    enabled: false,
-  },
   'users-permissions': {
     config: {
       jwtSecret: env('JWT_SECRET'),
